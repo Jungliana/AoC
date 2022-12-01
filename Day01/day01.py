d87317247c4baf04c5efd67195113ba1ffb25f47
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2022-12-01 08:18:58 +0100

    Day 01 - added type hints

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,19 +1,19 @@
 """Day01 - puzzle solutions for day 01."""
 
 
-def load_data(path):
+def load_data(path: str) -> list[str]:
     """Load and split data from file."""
-    with open(path, encoding=ascii) as file:
-        data = file.read()
-    return data.split("\n")
+    with open(path, encoding="ascii") as file:
+        lines = file.read()
+    return lines.split("\n")
 
 
-def sum_calories(data):
+def sum_calories(lines: list[str]) -> list[int]:
     """Sum calories for every elf."""
     calories = 0
     elf_calories = []
 
-    for number in data:
+    for number in lines:
         if number:
             calories += int(number)
         else:
@@ -22,17 +22,17 @@ def sum_calories(data):
     return elf_calories
 
 
-def solve_part1(numbers):
+def solve_part1(numbers: list[int]) -> int:
     """Solve part 1 of the puzzle."""
     return max(sum_calories(numbers))
 
 
-def solve_part2(numbers):
+def solve_part2(numbers: list[int]) -> int:
     """Solve part 2 of the puzzle."""
     return sum(sorted(sum_calories(numbers), reverse=True)[0:3])
 
 
 if __name__ == "__main__":
-    number_list = load_data("Day01/input01.txt")
-    print(f'Part 1 solution: {solve_part1(number_list)}')
-    print(f'Part 2 solution: {solve_part2(number_list)}')
+    data = load_data("Day01/input01.txt")
+    print(f'Part 1 answer: {solve_part1(data)}')
+    print(f'Part 2 answer: {solve_part2(data)}')
