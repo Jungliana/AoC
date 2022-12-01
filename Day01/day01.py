75c8aa3270b7e2abe3a4392a56ef6f95be37e4b5
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2022-12-01 07:20:50 +0100

    Day 01 stars 1 and 2 first version

---
+++ Day01/day01.py
@@ -0,0 +1,27 @@
+def load_data(path):
+    with open(path) as file:
+        data = file.readlines()
+    stripped = []
+    for element in data:
+        stripped.append(element.rstrip())
+    stripped.append('')
+    return stripped
+
+def sum_calories(data):
+    calories = 0
+    elf_calories = []
+
+    for number in data:
+        if number:
+            calories += int(number)
+        else:
+            elf_calories.append(calories)
+            calories = 0
+    return elf_calories
+
+
+if __name__ == "__main__":
+    numbers = load_data("Day01/input01.txt")
+    print(max(sum_calories(numbers)))
+    calories_list = sum_calories(numbers)
+    print(sum(sorted(calories_list, reverse=True)[0:3]))
