c2a8bcc0e55f114adbef8b795cf14cdb859c6166
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2022-12-01 07:53:49 +0100

    Day 01 - changes after installing hardcore linters

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,10 +1,15 @@
+"""Day01 - puzzle solutions for day 01."""
+
+
 def load_data(path):
-    with open(path) as file:
+    """Load and split data from file."""
+    with open(path, encoding=ascii) as file:
         data = file.read()
     return data.split("\n")
 
 
 def sum_calories(data):
+    """Sum calories for every elf."""
     calories = 0
     elf_calories = []
 
@@ -18,14 +23,16 @@ def sum_calories(data):
 
 
 def solve_part1(numbers):
+    """Solve part 1 of the puzzle."""
     return max(sum_calories(numbers))
 
 
 def solve_part2(numbers):
+    """Solve part 2 of the puzzle."""
     return sum(sorted(sum_calories(numbers), reverse=True)[0:3])
 
 
 if __name__ == "__main__":
-    numbers = load_data("Day01/input01.txt")
-    print(f'Part 1 solution: {solve_part1(numbers)}')
-    print(f'Part 2 solution: {solve_part2(numbers)}')
+    number_list = load_data("Day01/input01.txt")
+    print(f'Part 1 solution: {solve_part1(number_list)}')
+    print(f'Part 2 solution: {solve_part2(number_list)}')
