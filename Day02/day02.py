6dc952a90d4fd6000bc05d3a7ca6e6d4ab7b27bd
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2022-12-02 08:03:14 +0100

    Day 02 - solved part 2

--- Day02/day02.py
+++ Day02/day02.py
@@ -27,17 +27,38 @@ def point_dict(play_round):
 
 
 def solve_part1(letters):
+    """Solve part 1 of the puzzle."""
     result = 0
     for line in letters:
-        result += point_dict(line) 
+        result += point_dict(line)
     return result
 
 
+# A - rock (1); B - paper (2); C - scissors (3)
+# X, 0 - loss, Y - draw, Z - win
+def point_dict_2(play_round):
+    """Read round result from dictionary."""
+    points = {'A X': 3,
+              'B X': 1,
+              'C X': 2,
+              'A Y': 4,
+              'B Y': 5,
+              'C Y': 6,
+              'A Z': 8,
+              'B Z': 9,
+              'C Z': 7}
+    return points[play_round]
+
+
 def solve_part2(letters):
-    pass
+    """Solve part 2 of the puzzle."""
+    result = 0
+    for line in letters:
+        result += point_dict_2(line)
+    return result
 
 
 if __name__ == "__main__":
     data = load_data("Day02/input02.txt")
     print(f'Part 1 answer: {solve_part1(data)}')
-    # print(f'Part 2 answer: {solve_part2(data)}')
+    print(f'Part 2 answer: {solve_part2(data)}')
