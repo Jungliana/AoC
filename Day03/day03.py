8c202ac56706bd783bce9207ea1a60be603ecfb7
Jungliana <czasula@gmail.com>
2022-12-03 09:43:08 +0100

    Day 03 - added docstrings

--- Day03/day03.py
+++ Day03/day03.py
@@ -14,13 +14,15 @@ def load_data(path: str) -> list[str]:
 
 
 def find_double_points(backpack):
+    """Find common item in two backpack's halves."""
     backpack1 = backpack[:round(len(backpack)/2)]
     backpack2 = backpack[round(len(backpack)/2):len(backpack)]
     common = SequenceMatcher(None, backpack1, backpack2).find_longest_match()
     return LETTER_LIST.index(backpack[common.a])+1
 
 
-def sum_points(all_backpacks):
+def solve_part1(all_backpacks):
+    """Solve part 1 of the puzzle."""
     points = 0
     for backpack in all_backpacks:
         points += find_double_points(backpack)
@@ -28,21 +30,24 @@ def sum_points(all_backpacks):
 
 
 def find_badge(back1, back2, back3):
+    """Find common item in 3 backpacks."""
     for item in back1:
         if (item in back2) and (item in back3):
             return LETTER_LIST.index(item)+1
 
 
 def solve_part2(all_backpacks):
+    """Solve part 2 of the puzzle."""
     i = 0
     points = 0
     while i < len(all_backpacks):
-        points += find_badge(all_backpacks[i], all_backpacks[i+1], all_backpacks[i+2])
+        points += find_badge(all_backpacks[i], all_backpacks[i+1],
+                             all_backpacks[i+2])
         i += 3
     return points
 
 
 if __name__ == "__main__":
     data = load_data("Day03/input03.txt")
-    print(f'Part 1 answer: {sum_points(data)}')
+    print(f'Part 1 answer: {solve_part1(data)}')
     print(f'Part 2 answer: {solve_part2(data)}')
