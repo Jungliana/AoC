f76b0e21b9273c2190a3ca60e4f723490dec399b
Jungliana <czasula@gmail.com>
2022-12-04 09:31:37 +0100

    Day 04 - solved part 1

--- Day04/day04.py
+++ Day04/day04.py
@@ -6,11 +6,31 @@ def load_data(path: str) -> list[str]:
     lines = []
     with open(path, encoding="ascii") as file:
         for line in file:
-            lines.append(line.rstrip())
+            lines.append(line.rstrip().split(','))
     return lines
+
+
+def get_ints(ranges):
+    """Change ranges in string to ints."""
+    x = ranges[0].split('-')
+    y = ranges[1].split('-')
+    return (int(x[0]), int(x[1]), int(y[0]), int(y[1]))
+
+
+def check_range_in_other(ranges) -> bool:
+    if ranges[0] >= ranges[2] and ranges[1] <= ranges[3]:
+        return True
+    return (ranges[2] >= ranges[0] and ranges[3] <= ranges[1])
+
+
+def solve_part1(range_list):
+    fully_contains = 0
+    for assignment in range_list:
+        fully_contains += 1 if check_range_in_other(get_ints(assignment)) else 0
+    return fully_contains
 
 
 if __name__ == "__main__":
     data = load_data("Day04/input04.txt")
-    # print(f'Part 1 answer: {solve_part1(data)}')
+    print(f'Part 1 answer: {solve_part1(data)}')
     # print(f'Part 2 answer: {solve_part2(data)}')
