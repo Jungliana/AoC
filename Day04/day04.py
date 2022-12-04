9f9c14b71fcbafd2fad3b3c96d6a483f7469f6cf
Jungliana <czasula@gmail.com>
2022-12-04 09:43:45 +0100

    Day 04 - solved part 2

--- Day04/day04.py
+++ Day04/day04.py
@@ -10,27 +10,47 @@ def load_data(path: str) -> list[str]:
     return lines
 
 
-def get_ints(ranges):
+def get_ints(ranges: list[str]) -> tuple[int]:
     """Change ranges in string to ints."""
-    x = ranges[0].split('-')
-    y = ranges[1].split('-')
-    return (int(x[0]), int(x[1]), int(y[0]), int(y[1]))
+    r_1 = ranges[0].split('-')
+    r_2 = ranges[1].split('-')
+    return (int(r_1[0]), int(r_1[1]), int(r_2[0]), int(r_2[1]))
 
 
-def check_range_in_other(ranges) -> bool:
+def range_in_other(ranges: tuple[int]) -> bool:
+    """Check if one of the ranges contains the other."""
     if ranges[0] >= ranges[2] and ranges[1] <= ranges[3]:
         return True
     return (ranges[2] >= ranges[0] and ranges[3] <= ranges[1])
 
 
-def solve_part1(range_list):
+def solve_part1(range_list: list[list[str]]) -> int:
+    """Solve part 1 of the puzzle."""
     fully_contains = 0
     for assignment in range_list:
-        fully_contains += 1 if check_range_in_other(get_ints(assignment)) else 0
+        fully_contains += 1 if range_in_other(get_ints(assignment)) else 0
+    return fully_contains
+
+
+def ranges_overlap(ranges: tuple[int]) -> bool:
+    """Check if two ranges overlap."""
+    first = range(ranges[0], ranges[1]+1)
+    second = range(ranges[2], ranges[3]+1)
+    for number in first:
+        if number in second:
+            return True
+    return False
+
+
+def solve_part2(range_list: list[list[str]]) -> int:
+    """Solve part 2 of the puzzle."""
+    fully_contains = 0
+    for assignment in range_list:
+        fully_contains += 1 if ranges_overlap(get_ints(assignment)) else 0
     return fully_contains
 
 
 if __name__ == "__main__":
     data = load_data("Day04/input04.txt")
     print(f'Part 1 answer: {solve_part1(data)}')
-    # print(f'Part 2 answer: {solve_part2(data)}')
+    print(f'Part 2 answer: {solve_part2(data)}')
