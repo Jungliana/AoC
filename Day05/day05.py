fa62635ab2cd99f920ccf1b1b858e92d9c2bce65
Jungliana <czasula@gmail.com>
2022-12-05 08:11:16 +0100

    Day 05 - added Crane class, solved part 1

--- Day05/day05.py
+++ Day05/day05.py
@@ -6,11 +6,54 @@ def load_data(path: str) -> list[str]:
     lines = []
     with open(path, encoding="ascii") as file:
         for line in file:
-            lines.append(line.rstrip())
+            lines.append(line.split())
     return lines
 
 
+class Crane():
+    """Crane for moving crates."""
+
+    def __init__(self) -> None:
+        """Construct a crane with starting crate configuration."""
+        self.crates = [
+            [],
+            ['H', 'C', 'R'],
+            ['B', 'J', 'H', 'L', 'S', 'F'],
+            ['R', 'M', 'D', 'H', 'J', 'T', 'Q'],
+            ['S', 'G', 'R', 'H', 'Z', 'B', 'J'],
+            ['R', 'P', 'F', 'Z', 'T', 'D', 'C', 'B'],
+            ['T', 'H', 'C', 'G'],
+            ['S', 'N', 'V', 'Z', 'B', 'P', 'W', 'L'],
+            ['R', 'J', 'Q', 'G', 'C'],
+            ['L', 'D', 'T', 'R', 'H', 'P', 'F', 'S']
+        ]
+
+    def move_crates(self, many, m_from, m_to):
+        """Move x crates from one stack to another."""
+        for _ in range(many):
+            crate = self.crates[m_from].pop()
+            self.crates[m_to].append(crate)
+
+    def move_all(self, lines):
+        """Move all crates according to the instructions."""
+        for line in lines:
+            self.move_crates(int(line[1]), int(line[3]), int(line[5]))
+
+    def read_message(self):
+        """Read message from crate stacks."""
+        message = ''
+        for i in range(1, 10):
+            message += self.crates[i].pop()
+        return message
+
+    def solve_part1(self, lines):
+        """Solve part 1 of the puzzle."""
+        self.move_all(lines)
+        return self.read_message()
+
+
 if __name__ == "__main__":
-    data = load_data("Day05/input05.txt")
-    # print(f'Part 1 answer: {solve_part1(data)}')
+    data = load_data("Day05/input05_b.txt")
+    crane1 = Crane()
+    print(f'Part 1 answer: {crane1.solve_part1(data)}')
     # print(f'Part 2 answer: {solve_part2(data)}')
