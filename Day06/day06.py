e2034a53a842fa13f3cbfb55def9dba926284a73
Jungliana <czasula@gmail.com>
2022-12-06 09:45:26 +0100

    Day 06 - (very bad) part 1 solution

--- Day06/day06.py
+++ Day06/day06.py
@@ -7,7 +7,25 @@ def load_data(path: str) -> str:
         return file.readlines()[0]
 
 
+def all_charas_different(token):
+    return (token[0] not in token[1:]) and (token[-1] not in token[:-1]) and (token[1] != token[2])
+
+
+def solve_part1(stream):
+    que = list(stream[0:4])
+    counter = 4
+    if all_charas_different(que):
+        return counter
+    for character in stream[4:]:
+        counter += 1
+        que.pop(0)
+        que.append(character)
+        if all_charas_different(que):
+            return counter
+    return counter
+
+
 if __name__ == "__main__":
     data = load_data("Day06/input06.txt")
-    # print(f'Part 1 answer: {solve_part1(data)}')
+    print(f'Part 1 answer: {solve_part1(data)}')
     # print(f'Part 2 answer: {solve_part2(data)}')
