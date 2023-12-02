88c96a650c86e0aac39bccdb2fb0c5f77ee6a159
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2023-12-02 18:04:02 +0100

    Day 02 part2 refactor

--- AoC-2023/Day02/day02.py
+++ AoC-2023/Day02/day02.py
@@ -1,10 +1,7 @@
 class Game:
     def __init__(self, game_id: int) -> None:
-        self.id = game_id
+        self.id: int = game_id
         self.cubes = {"red": [], "green": [], "blue": []}
-
-    def __str__(self) -> str:
-        return f'Game: {self.id}'
 
     def id_game_possible(self, all_cubes: list[tuple]) -> int:
         for cube_set in all_cubes:
@@ -12,47 +9,40 @@ class Game:
                 return 0
         return self.id
 
-    def set_power(self) -> int:
+    def power_of_set(self) -> int:
         power = 1
         for cube_values in self.cubes.values():
             power *= max(cube_values)
         return power
 
 
-def read_data(path: str) -> list[str]:
+def read_data(path: str) -> list[Game]:
+    lines = []
     with open(path, encoding="ASCII") as file:
-        lines = file.readlines()
+        for line in file:
+            lines.append(process_line_of_data(line))
     return lines
 
 
 def process_line_of_data(line: str) -> Game:
-    # Game 1: 10 green, 9 blue, 1 red; 1 red, 7 green; 11 green, 6 blue; 8 blue, 12 green
     games = line.rstrip("\n").split(":")
     game_id = int(games[0].split()[1])
     game_obj = Game(game_id)
-    games_splitted = games[1].split(";")
-    for game in games_splitted:
-        cubes_split = game.split(",")
+    examples_split = games[1].split(";")
+    for example in examples_split:
+        cubes_split = example.split(",")
         for element in cubes_split:
             element = element.split()
             game_obj.cubes[element[1]].append(int(element[0]))
     return game_obj
 
 
-def process_data(data: list[str]) -> list[Game]:
-    return [process_line_of_data(line) for line in data]
-
-
 def solve_part1(data, tuples):
-    data = process_data(data)
-    ids = [game.id_game_possible(tuples) for game in data]
-    return sum(ids)
+    return sum(game.id_game_possible(tuples) for game in data)
 
 
 def solve_part2(data):
-    data = process_data(data)
-    powers = [game.set_power() for game in data]
-    return sum(powers)
+    return sum(game.power_of_set() for game in data)
 
 
 if __name__ == "__main__":
