2b575091d953bb8147c1d202512444bb15968c51
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2023-12-02 17:54:42 +0100

    AoC 2023 day 02 part2 solution, better part1

--- AoC-2023/Day02/day02.py
+++ AoC-2023/Day02/day02.py
@@ -1,18 +1,22 @@
 class Game:
-    def __init__(self, game_id: int, games: list[list[tuple]]) -> None:
+    def __init__(self, game_id: int) -> None:
         self.id = game_id
-        self.games = games
+        self.cubes = {"red": [], "green": [], "blue": []}
 
     def __str__(self) -> str:
         return f'Game: {self.id}'
 
     def id_game_possible(self, all_cubes: list[tuple]) -> int:
-        for example in self.games:
-            for color in example:
-                for cubes in all_cubes:
-                    if (color[0] == cubes[0]) and (color[1] > cubes[1]):
-                        return 0
+        for cube_set in all_cubes:
+            if max(self.cubes[cube_set[0]]) > cube_set[1]:
+                return 0
         return self.id
+
+    def set_power(self) -> int:
+        power = 1
+        for cube_values in self.cubes.values():
+            power *= max(cube_values)
+        return power
 
 
 def read_data(path: str) -> list[str]:
@@ -25,13 +29,14 @@ def process_line_of_data(line: str) -> Game:
     # Game 1: 10 green, 9 blue, 1 red; 1 red, 7 green; 11 green, 6 blue; 8 blue, 12 green
     games = line.rstrip("\n").split(":")
     game_id = int(games[0].split()[1])
+    game_obj = Game(game_id)
     games_splitted = games[1].split(";")
-    all_games = []
     for game in games_splitted:
-        cubes = game.split(",")
-        all_games.append([(details.split()[1], int(details.split()[0])) for details in cubes])
-    # [[(10, "green"), (9, "blue"), (1, "red")], [(1, "red"), (7, "green")]]
-    return Game(game_id, all_games)
+        cubes_split = game.split(",")
+        for element in cubes_split:
+            element = element.split()
+            game_obj.cubes[element[1]].append(int(element[0]))
+    return game_obj
 
 
 def process_data(data: list[str]) -> list[Game]:
@@ -41,15 +46,17 @@ def process_data(data: list[str]) -> list[Game]:
 def solve_part1(data, tuples):
     data = process_data(data)
     ids = [game.id_game_possible(tuples) for game in data]
-    print(ids)
     return sum(ids)
 
 
 def solve_part2(data):
-    pass
+    data = process_data(data)
+    powers = [game.set_power() for game in data]
+    return sum(powers)
 
 
 if __name__ == "__main__":
     data_input = read_data("AoC-2023/Day02/input02.txt")
-    print(f'Part 1 solution: {solve_part1(data_input, [("red", 12), ("green", 13), ("blue", 14)])}')
+    color_nums = [("red", 12), ("green", 13), ("blue", 14)]
+    print(f'Part 1 solution: {solve_part1(data_input, color_nums)}')
     print(f'Part 2 solution: {solve_part2(data_input)}')
