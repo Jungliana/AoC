b44d22add1c796894b69192b209c6e7474654e90
apiekarz <a.piekarzewicz@stud.elka.pw.edu.pl>
2023-12-04 08:08:08 +0100

    Extract method count_winning for day 04

--- AoC-2023/Day04/day04.py
+++ AoC-2023/Day04/day04.py
@@ -9,17 +9,22 @@ def read_data(path: str) -> list[list[str]]:
     return lines
 
 
+def count_winning(line):
+    all_nums = line.split(" | ")
+    winning = set()
+    winning.update([int(number) for number in all_nums[0].split()])
+
+    have = set()
+    have.update([int(number) for number in all_nums[1].split()])
+
+    have_winning = len(winning.intersection(have))
+    return have_winning
+
+
 def solve_part1(data):
     points = 0
     for line in data:
-        all_nums = line[1].split(" | ")
-        winning = set()
-        winning.update([int(number) for number in all_nums[0].split()])
-
-        have = set()
-        have.update([int(number) for number in all_nums[1].split()])
-
-        if (point_power := len(winning.intersection(have))-1) >= 0:
+        if (point_power := count_winning(line[1])-1) >= 0:
             points += 2 ** point_power
     return points
 
@@ -30,15 +35,7 @@ def solve_part2(data):
         card_id = int(line[0].split()[1])
         all_copies[card_id] += 1
 
-        all_nums = line[1].split(" | ")
-        winning = set()
-        winning.update([int(number) for number in all_nums[0].split()])
-
-        have = set()
-        have.update([int(number) for number in all_nums[1].split()])
-
-        copies = len(winning.intersection(have))
-
+        copies = count_winning(line[1])
         for i in range(card_id+1, card_id+copies+1):
             all_copies[i] += all_copies[card_id]
 
